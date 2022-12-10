64d1448e9c976141c43b4f9a6c97ebb1232d16d5
bmcnamee <860221+bmcnamee@users.noreply.github.com>
2022-12-10 12:04:27 -0800

    added sample metric

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
 import streamlit as st  # loaded by streamlit cloud
 
 st.markdown(f"# BIMO BI 2")
-st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+st.metric(label="# Overall Census<br>Metric description here", value="92%", delta="1.2")
