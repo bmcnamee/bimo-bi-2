e6953510c48fc898eb1a0826227ef83e5ade854f
bmcnamee <860221+bmcnamee@users.noreply.github.com>
2022-12-10 12:05:30 -0800

    updated sample metric

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
 import streamlit as st  # loaded by streamlit cloud
 
 st.markdown(f"# BIMO BI 2")
-st.metric(label="# Overall Census<br>Metric description here", value="92%", delta="1.2")
+st.metric(label="Overall Census<br>Metric description here", value="92%", delta="1.2")
