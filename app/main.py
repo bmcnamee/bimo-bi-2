3b8b66989af9a10d7cf537fba2283dc660cd93bb
bmcnamee <860221+bmcnamee@users.noreply.github.com>
2022-12-10 12:19:07 -0800

    fixed metric text and added config file with sample theme

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import streamlit as st  # loaded by streamlit cloud
 
 st.markdown(f"# BIMO BI 2")
-st.metric(label=st.markdown("Overall Census<br>Metric description here"), value="92%", delta="1.2")
-st.metric(label=st.markdown("# Metric 2"), value="92%", delta="1.2")
+st.metric(label="Overall Census", value="92%", delta="1.2")
+st.metric(label="Metric 2", value="92%", delta="1.2")
