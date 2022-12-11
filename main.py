51057c2c367acc13e95f51798299e9a80b129de1
bmcnamee <860221+bmcnamee@users.noreply.github.com>
2022-12-11 08:51:07 -0800

    added metric summary table to kpi summary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st  # loaded by streamlit cloud
+import pandas as pd # loaded by streamlit cloud
 
 st.set_page_config(
     page_title="Executive Dashboard Summary",
@@ -39,6 +40,7 @@ tabs_list = [
 t1, t2, t3, t4, t5 = st.tabs(tabs_list)
 
 with t1:
+    # Metrics
     r1c1, r1c2, r1c3, r1c4 = st.columns(4)
     r1c1.metric(label="Overall Census", value="92.0%", help="Percent of beds currently filled.")
     r1c2.metric(label="ER Waiting", value="8 patients", help="Number of patients currently waiting in the ER.")
@@ -58,6 +60,33 @@ with t1:
     r2c4.metric(label="30 Day Re-admit Rate", value="2.2%", delta="1.0 MoM"
                 , help="Current month to date 30 day patient re-admittance rate \
                         and absolute difference from prior month.")
+
+    # Measure Summary
+    measure_summary_data = []
+    measure_summary_row = {
+        "type":"Quality",
+        "measure":"30 Day Re-admissions",
+        "mtd":"2.5%",
+        "mtd_target":"5.9%",
+        "mtd_pct_from_target":"58.0%",
+        "ytd":"37.5%",
+        "ytd_target":"7.3%",
+        "ytd_pct_from_target":"-411.6%"
+    }
+    measure_summary_data.append(measure_summary_row)
+    measure_summary_row = {
+        "type":"Efficiency",
+        "measure":"Avg Inpatient LOS",
+        "mtd":"3.7",
+        "mtd_target":"3.7",
+        "mtd_pct_from_target":"0.3%",
+        "ytd":"3.8",
+        "ytd_target":"3.8",
+        "ytd_pct_from_target":"0.1%"
+    }
+    measure_summary_data.append(measure_summary_row)
+    df_ms = pd.DataFrame(measure_summary_data)
+    st.table(data=df_ms)
 
 with t2:
     st.markdown(f"{tabs_list[1]}")
