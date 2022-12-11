41d1fdc42602a0fefec5e3e1105a3d1a455aa251
bmcnamee <860221+bmcnamee@users.noreply.github.com>
2022-12-10 17:29:59 -0800

    center metric content and update css

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ def local_css(file_name: str) -> None:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
-def remote_css(url: str) -> None:
-    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
-
-
-def icon(icon_name: str) -> None:
-    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+# def remote_css(url: str) -> None:
+#     st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+#
+#
+# def icon(icon_name: str) -> None:
+#     st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
 
 
 local_css("style.css")
-remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
-icon("search")
 
+# remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+# icon("search")
+# selected = st.text_input("", "Search...")
+# button_clicked = st.button("OK")
 
-selected = st.text_input("", "Search...")
-button_clicked = st.button("OK")
-st.markdown(f"# BIMO BI 2")
+st.markdown(f"# Executive Summary Dashboard")
 
 
 r1c1, r1c2, r1c3, r1c4 = st.columns(4)
