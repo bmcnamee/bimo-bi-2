fece6e48eee321b249d9d95c4adca7fb5415d44d
bmcnamee <860221+bmcnamee@users.noreply.github.com>
2022-12-10 18:27:03 -0800

    added tabs

--- main.py
+++ main.py
@@ -21,26 +21,46 @@ local_css("style.css")
 # selected = st.text_input("", "Search...")
 # button_clicked = st.button("OK")
 
-st.markdown(f"# Executive Summary Dashboard")
+st.markdown("# Executive Summary Dashboard")
 
+tabs_list = [
+    "KPI Summary",
+    "Domain Scores",
+    "Patient Satisfaction",
+    "Patient Rounding",
+    "Service Recovery",
+]
+t1, t2, t3, t4, t5 = st.tabs(tabs_list)
 
-r1c1, r1c2, r1c3, r1c4 = st.columns(4)
-r1c1.metric(label="Overall Census", value="92.0%", help="Percent of beds currently filled.")
-r1c2.metric(label="ER Waiting", value="8 patients", help="Number of patients currently waiting in the ER.")
-r1c3.metric(label="Admitted Today", value="3 patients", help="Number of patients admitted so far today.")
-r1c4.metric(label="Discharged Today", value="3 patients", help="Number of patients discharged so far today.")
+with t1:
+    r1c1, r1c2, r1c3, r1c4 = st.columns(4)
+    r1c1.metric(label="Overall Census", value="92.0%", help="Percent of beds currently filled.")
+    r1c2.metric(label="ER Waiting", value="8 patients", help="Number of patients currently waiting in the ER.")
+    r1c3.metric(label="Admitted Today", value="3 patients", help="Number of patients admitted so far today.")
+    r1c4.metric(label="Discharged Today", value="3 patients", help="Number of patients discharged so far today.")
 
+    r2c1, r2c2, r2c3, r2c4 = st.columns(4)
+    r2c1.metric(label="Overall Patient Satisfaction", value="77.6%", delta="1.2 MoM"
+                , help="Current month to date overall patient satisfaction percent \
+                        and absolute difference from prior month.")
+    r2c2.metric(label="Likelihood to Recommend", value="77.6%", delta="-0.6 MoM"
+                , help="Current month to date likelihood to recommend percent \
+                        and absolute difference from prior month.")
+    r2c3.metric(label="Length of Stay", value="4.2 days", delta="1.2 MoM"
+                , help="Current month to date average length of patient stay in days \
+                        and absolute difference from prior month.")
+    r2c4.metric(label="30 Day Re-admit Rate", value="2.2%", delta="1.0 MoM"
+                , help="Current month to date 30 day patient re-admittance rate \
+                        and absolute difference from prior month.")
 
-r2c1, r2c2, r2c3, r2c4 = st.columns(4)
-r2c1.metric(label="Overall Patient Satisfaction", value="77.6%", delta="1.2 MoM"
-            , help="Current month to date overall patient satisfaction percent \
-                    and absolute difference from prior month.")
-r2c2.metric(label="Likelihood to Recommend", value="77.6%", delta="-0.6 MoM"
-            , help="Current month to date likelihood to recommend percent \
-                    and absolute difference from prior month.")
-r2c3.metric(label="Length of Stay", value="4.2 days", delta="1.2 MoM"
-            , help="Current month to date average length of patient stay in days \
-                      and absolute difference from prior month.")
-r2c4.metric(label="30 Day Re-admit Rate", value="2.2%", delta="1.0 MoM"
-            , help="Current month to date 30 day patient re-admittance rate \
-                      and absolute difference from prior month.")
+with t2:
+    st.markdown(f"{tabs_list[1]}")
+
+with t3:
+    st.markdown(f"{tabs_list[2]}")
+
+with t4:
+    st.markdown(f"{tabs_list[3]}")
+
+with t5:
+    st.markdown(f"{tabs_list[4]}")
